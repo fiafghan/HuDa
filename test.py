0460720b91e7a1fdbd8dc11528c2d30dee9d8d77
fiafghan <fiafghan@gmail.com>
2025-10-04 15:37:10 +0430

    median was also tested!

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-from huda.cleaning import fill_mean
+from huda.cleaning import fill_median
 from huda.csv import open_csv
 
 df = open_csv("testdata/test.csv")
 
-meaned_df = fill_mean(df, column="population")
+meaned_df = fill_median(df, column="population")
 meaned_df.write_csv("testdata/test1.csv")
 print(meaned_df)
