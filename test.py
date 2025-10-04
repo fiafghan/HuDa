3814d106d41d6c40f012a68e904d0b5227d134d5
fiafghan <fiafghan@gmail.com>
2025-10-04 15:31:46 +0430

    fill_mean.py was also tested!

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
-from huda.cleaning import drop_missing
+from huda.cleaning import fill_mean
 from huda.csv import open_csv
 
 df = open_csv("testdata/test.csv")
 
-cleaned_df = drop_missing(df)
-cleaned_df = cleaned_df.write_csv("testdata/cleaned_data.csv")
-print(cleaned_df)
+meaned_df = fill_mean(df, column="population")
+meaned_df.write_csv("testdata/test1.csv")
+print(meaned_df)
