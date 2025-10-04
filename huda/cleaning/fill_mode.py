bd2ba499c5e66842f09d2086fbdf0bf80dd67ed3
fiafghan <fiafghan@gmail.com>
2025-10-04 15:51:55 +0430

    fill_mode.py was tested! it was working 100%.

--- huda/cleaning/fill_mode.py
+++ huda/cleaning/fill_mode.py
@@ -6,11 +6,37 @@ def fill_mode(df, column):
 
     Example:
     ----------
-        df = pl.DataFrame({"city": ["Kabul", None, "Kabul", "Herat"]})
+        import polars as pl
+        from huda.missing.fill_mode import fill_mode
+
+        # Create a sample DataFrame
+        df = pl.DataFrame({
+            "city": ["Kabul", None, "Kabul", "Herat", None],
+            "year": [2020, 2021, 2020, 2022, 2021]
+        })
+
+        # Apply mode filling
         df_clean = fill_mode(df, "city")
+
+        # Output:
+        # ✅ Missing values in 'city' filled with mode (Kabul)
+        # shape: (5, 2)
+        # ┌────────┬──────┐
+        # │ city   ┆ year │
+        # │ ---    ┆ ---  │
+        # │ str    ┆ i64  │
+        # ╞════════╪══════╡
+        # │ Kabul  ┆ 2020 │
+        # │ Kabul  ┆ 2021 │
+        # │ Kabul  ┆ 2020 │
+        # │ Herat  ┆ 2022 │
+        # │ Kabul  ┆ 2021 │
+        # └────────┴──────┘
     """
     try:
-        mode_val = df[column].mode().to_list()[0]
+        # Calculate the most frequent (mode) value
+        mode_val = df[column].drop_nulls().mode().to_list()[0]
+        # Fill missing values with that mode
         df_clean = df.with_columns(pl.col(column).fill_null(mode_val))
         print(f"✅ Missing values in '{column}' filled with mode ({mode_val})")
         return df_clean
--- test.py
+++ test.py
@@ -1,8 +1,7 @@
-from huda.cleaning import fill_median
+from huda.cleaning import fill_mode
 from huda.csv import open_csv
 
 df = open_csv("testdata/test.csv")
 
-meaned_df = fill_median(df, column="population")
-meaned_df.write_csv("testdata/test1.csv")
-print(meaned_df)
+moded_df = fill_mode(df = df, column="population")
+print(moded_df)
