599fc259377e39161112db2bc46771a16328285f
fiafghan <fiafghan@gmail.com>
2025-10-05 11:17:03 +0430

    i created another folder and called it opening. and i put all files related to loading dataset like open csv, open json and others inside that.

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from huda.cleaning import fill_constant
-from huda.csv import open_csv
+from huda.opening import open_csv
 
 df = open_csv("testdata/test.csv")
 
-filled_constant = fill_constant(df, "Missing", "population")
+filled_constant = fill_constant(df, "Missing")
 print(filled_constant)
