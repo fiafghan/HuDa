a5445de9dd96016c29dc07ae9fb95c1e3d7673f6
fiafghan <fiafghan@gmail.com>
2025-10-07 22:08:24 +0430

    Remove time-based data aggregation functionality and update related references

--- huda/transformation/__init__.py
+++ huda/transformation/__init__.py
@@ -1,8 +1,6 @@
 from .region_based_data_aggregation import region_based_data_aggregation
-from .time_based_data_aggregration import time_based_data_aggregration
 
 
 __all__ = [
     "region_based_data_aggregation",
-    "time_based_data_aggregration",
 ]
--- test.py
+++ test.py
@@ -1,12 +1,13 @@
 from huda.opening import open_csv
 from huda.transformation import time_based_data_aggregration
+import polars as pl
 
 df = open_csv("testdata/agg_time.csv")
 
 print (df)
 
-
-agg = time_based_data_aggregration(df, frequency="monthly", date_column_name="date",aggregation_method="mean")
+agg = time_based_data_aggregration(df, frequency="monthly", date_column_name="date",aggregation_method="sum")
 
 print (agg)
+
 
