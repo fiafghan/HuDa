240b0814a58a14689ca79126dd0b18f160e76c35
fiafghan <fiafghan@gmail.com>
2025-10-08 11:39:01 +0430

    Update import for adults_children_ratios to adults_children_male_female_ratios in test.py

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 from huda.opening import open_csv
-from huda.transformation import adults_children_ratios
+from huda.transformation import adults_children_male_female_ratios
 import polars as pl
 
 df = open_csv("testdata/demographics.csv")
 
 print (df)
 
-dem = adults_children_ratios(df, numerator_columns=['male'], denominator_columns=['female'])
+dem = adults_children_male_female_ratios(df, numerator_columns=['male'], denominator_columns=['female'])
 
 print (dem)
 
